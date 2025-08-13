1cfd398cec390c3191e004c617b52dc808e4549c
marinaroncatto <marinam.roncatto@gmail.com>
2025-08-13 16:39:28 -0300

    About view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,41 +1,67 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
-public class MainViewController implements Initializable{
-	
+public class MainViewController implements Initializable {
+
 	@FXML
-	private MenuItem meuItemSeller;	
-	
+	private MenuItem meuItemSeller;
+
 	@FXML
 	private MenuItem meuItemDepartment;
-	
+
 	@FXML
 	private MenuItem meuItemAbout;
-	
+
 	@FXML
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemDepartmentAction() {
 		System.out.println("onMenuItemDepartmentAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
-	}		
-	
-	@Override
-	public void initialize(URL uri, ResourceBundle rb) {		 
-		
+		loadView("/gui/About.fxml");
 	}
 
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+
+	}
+
+	//synchronized garante que todo processamento multithread vai ocorrer sem ser interrompido
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox)((ScrollPane)mainScene.getRoot()).getContent(); //retorna o primeiro elemento da view
+			
+			Node mainMenu = mainVBox.getChildren().get(0); //primeiro filho
+			mainVBox.getChildren().clear(); //limpa de todos os filhos
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
 }
